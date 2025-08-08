20a65cf18f0c1b154f04bc0f0a4132bc90bfc243
unknown <vinicius_24019@ETEC.FURLAN>
2025-08-08 10:53:14 -0300

    commit

--- app/index.js
+++ app/index.js
@@ -1,12 +1,23 @@
-import {View, Text, Pressable, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 
 export default function Home(){
 
     return(
-        <View>
-
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello Word</Text>
         </View>
         
     )
 }
 
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title:{
+        fontSize: 30,
+        fontWeight:'bold',
+    }
+});
